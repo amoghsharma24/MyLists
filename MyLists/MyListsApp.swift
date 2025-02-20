410ccde91fa78d17b76034828eb38af4d0226f84
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-21 10:31:18 +1100

    Initial Commit

---
+++ MyLists/MyListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListsApp.swift
+//  MyLists
+//
+//  Created by Amogh Sharma  on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
