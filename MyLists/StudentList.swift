ae25bc31e7a6430ad6aa3d7e06b639e99b83fbce
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-21 11:27:24 +1100

    Completed Lists and navigation stacks

---
+++ MyLists/StudentList.swift
@@ -0,0 +1,18 @@
+//
+//  StudentList.swift
+//  MyLists
+//
+//  Created by Amogh Sharma  on 21/2/2025.
+//
+
+import SwiftUI
+
+struct StudentList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentList()
+}
