2c8def5b0379a602a205d01c31569d9123e6d4f2
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-21 13:15:04 +1100

    Made a working login page well workingbut not lmao

---
+++ LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MyLists
+//
+//  Created by Amogh Sharma  on 21/2/2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
